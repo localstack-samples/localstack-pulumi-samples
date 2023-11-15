497f59ae5e0c5ce409140f39630196596ca2b992
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-11-15 22:35:25 +0100

    Migrate to AWS_ENDPOINT_URL

--- lambda-kinesis-firehose-es/lambda.py
+++ lambda-kinesis-firehose-es/lambda.py
@@ -9,11 +9,11 @@ logger = logging.getLogger(__name__)
 def handler(event, context):
     try:
         endpoint_url = None
-        # boto instrumentation is a pro feature (https://docs.localstack.cloud/tools/local-endpoint-injection/)
+        # Automatic boto instrumentation is a pro feature https://docs.localstack.cloud/user-guide/tools/transparent-endpoint-injection/
         # we'll do this manually here
-        if os.environ.get("LOCALSTACK_HOSTNAME"):
-            endpoint_url = "http://{}:{}".format(os.environ["LOCALSTACK_HOSTNAME"], os.environ["EDGE_PORT"])
-            
+        if os.environ.get("AWS_ENDPOINT_URL"):
+            endpoint_url = os.environ["AWS_ENDPOINT_URL"]
+
         kinesis = boto3.client(
             "kinesis", endpoint_url=endpoint_url, region_name=os.environ["AWS_REGION"], verify=False
         )
@@ -22,5 +22,5 @@ def handler(event, context):
         logger.info("Put record in stream %s.", stream_name)
     except Exception:
         logger.exception("Sending record to kinesis failed.")
-    
+
     return {"body": "Hello World!"}
